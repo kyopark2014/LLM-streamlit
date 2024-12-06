31aa081a49db137b30a3aaf17091db540665903a
Kyoungsu Park <kyopark2014@gmail.com>
2024-12-06 11:34:01 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -120,6 +120,16 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       'httpIpv4',
+    );
+    ec2SecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'httpIpv4',
+    );
+    ec2SecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8501),
+      'httpIpv4',
     ); 
 
     // set AMI
