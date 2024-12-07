7cde23574177b00b889ebba2eadac98a9c62afd1
Kyoungsu Park <kyopark2014@gmail.com>
2024-12-07 09:20:09 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -113,8 +113,7 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
     //   vpcSubnets: {
     //     subnets: vpc.publicSubnets
     //   },
-    //   securityGroup: albSg,
-    //   loadBalancerName: `alb-${projectName}`,
+    //   securityGroup: albSg
     // })
     // ec2SecurityGroup.connections.allowFrom(albSg, ec2.Port.tcp(8501), 'allow http traffic from alb')
 
@@ -193,27 +192,9 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
 
     const targets: elbv2_tg.InstanceTarget[] = new Array();
     targets.push(new elbv2_tg.InstanceTarget(appInstance));
+    
 
-
-    // const nlbSg = new ec2.SecurityGroup(this, `nlb-sg-for-${projectName}`, {
-    //   vpc,
-    //   allowAllOutbound: true,
-    //   description: 'security group for nlb'
-    // })
-    // nlbSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anyone')
-
-    // const nlbSg = new ec2.SecurityGroup(this, `NLBSecurityGroup-${projectName}`, { 
-    //   vpc 
-    // });
-
-    const nlb = new elbv2.NetworkLoadBalancer(this, `lb-for-${projectName}`, { 
-      vpc,
-      // vpcSubnets: {
-      //   subnets: vpc.publicSubnets
-      // },
-      loadBalancerName: `nlb-${projectName}`,
-      // securityGroups: [nlbSg]
-    });
+    const nlb = new elbv2.NetworkLoadBalancer(this, `lb-for-${projectName}`, { vpc });
 
     const listener = nlb.addListener(`listener-${projectName}`, { port: 80 });
     listener.addTargets('target', {
