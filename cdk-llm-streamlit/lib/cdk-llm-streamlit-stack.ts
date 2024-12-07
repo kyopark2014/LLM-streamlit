84f9da0c5e99e4904f087eb6d5607f9dd5291931
Kyoungsu Park <kyopark2014@gmail.com>
2024-12-07 16:14:39 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -99,11 +99,11 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
       ec2.Port.tcp(22),
       'SSH',
     );
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      'HTTP',
-    );
+    // ec2SecurityGroup.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(80),
+    //   'HTTP',
+    // );
 
     // ALB    
     const albSg = new ec2.SecurityGroup(this, `alb-sg-for-${projectName}`, {
@@ -113,7 +113,7 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
       description: 'security group for alb'
     })
     albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anyone')
-
+    
     const alb = new elbv2.ApplicationLoadBalancer(this, `alb-for-${projectName}`, {
       internetFacing: true,
       vpc,
