ccf35f858cd15bbe22408b2430935ff789122465
Kyoungsu Park <kyopark2014@gmail.com>
2024-12-08 14:04:27 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -116,7 +116,7 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
     // albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anyone') // internet -> alb
     
     const alb = new elbv2.ApplicationLoadBalancer(this, `alb-for-${projectName}`, {
-      internetFacing: true,
+      // internetFacing: true,
       vpc: vpc,
       vpcSubnets: {
         subnets: vpc.publicSubnets
