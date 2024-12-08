59a74203bfb0c15930b48567c8ffe4dd63c80d10
Kyoungsu Park <kyopark2014@gmail.com>
2024-12-08 13:26:26 +0900

    update

--- cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
+++ cdk-llm-streamlit/lib/cdk-llm-streamlit-stack.ts
@@ -114,12 +114,17 @@ export class CdkLlmStreamlitStack extends cdk.Stack {
     })
     ec2Sg.connections.allowFrom(albSg, ec2.Port.tcp(80), 'allow http traffic from alb') // alb -> ec2
     // albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anyone') // internet -> alb
+    albSg.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'SSH',
+    );
     
     const alb = new elbv2.ApplicationLoadBalancer(this, `alb-for-${projectName}`, {
       internetFacing: true,
       vpc: vpc,
       vpcSubnets: {
-        subnets: vpc.privateSubnets // publicSubnets
+        subnets: vpc.publicSubnets
       },
       securityGroup: albSg,
       loadBalancerName: `alb-for-${projectName}`
